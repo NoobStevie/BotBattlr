7fb65915da30ac6741e0fc3b7ccbe9a0cf783914
Steve <kamaustevek78@gmail.com>
2023-05-01 14:59:22 +0300

    Completed Assignment

---
+++ botbattlr/src/containers/BotCollection.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import BotCard from '../components/BotCard'
+
+class BotCollection extends Component {
+    // your code here
+
+    render() {
+        return (
+            <div className="ui four column grid">
+                <div className="row">
+                    {this.props.bots.map(bot => <BotCard bot={bot} handleBot={this.props.addBot} dischargeForever={this.props.dischargeForever}/>)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BotCollection;
